29a753b3a387c561263b0a360615234c3eab58a0
Erick Ochieng <erickochieng766@gmail.com>
2023-02-12 19:54:44 +0300

    Logo

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
+import "./Title.css"
+
+import logo2 from "../assets/logo3.png"
+
 function Title() {
 
     const [span, setSpan] = useState(1)
 
     return (
-        <div>
+        <div className='title'>
+            <div className='logo'>
+                <img src={logo2} alt="Logo" />
+            </div>
             <h1><span className={ span === 0 ? "clip-text" : "one" }>Twenty.</span><span className={ span === 1 ? "clip-text" : "two" }>Twenty.</span><span className={ span === 2 ? "clip-text" : "three" }>Three</span></h1>
         </div>
     )
